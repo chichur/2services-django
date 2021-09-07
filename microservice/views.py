7cc6e9d041683f5896d12cc50c2826a5836e71dd
chichur <chichur@physicon.ru>
2021-09-07 13:14:57 +0300

    Готово без тональности

--- microservice/views.py
+++ microservice/views.py
@@ -27,33 +27,29 @@ def micro_service1(request):
 
                 # ищем эмэилы
                 emails = re.findall(re_email, content)
-                data['emails'] = emails
+                data['email'] = emails
 
                 # ищем ФИО
                 last_names = re.findall(re_lastname, content)
-                data['last_names'] = last_names
+                data['lastname'] = last_names
 
                 # ищем компании
                 company = re.findall(re_company, content)
                 data['company'] = company
 
-                for value in data['emails']:
-                    url_data = UrlData()
-                    url_data.url = url
-                    url_data.email = value
-                    url_data.save()
+                data['negative_header'] = []
 
-                for value in data['last_names']:
-                    url_data = UrlData()
-                    url_data.url = url
-                    url_data.lastname = value
-                    url_data.save()
+                for key in data.keys():
+                    if key == 'url':
+                        continue
+                    for value in data[key]:
+                        url_data = UrlData()
+                        url_data.url = url
+                        setattr(url_data, key, value)
 
-                for value in data['company']:
-                    url_data = UrlData()
-                    url_data.url = url
-                    url_data.company = value
-                    url_data.save()
+                        if UrlData.objects.filter(**{key: value}).exists():
+                            continue
+                        url_data.save()
 
                 return Response(data)
             else:
@@ -65,4 +61,24 @@ def micro_service1(request):
 @api_view(['GET'])
 def micro_service2(request):
     if request.method == 'GET':
-        return Response(request.query_params)
+        if request.query_params.get('key'):
+            if request.query_params['key'] == key_access:
+                lastname = request.query_params.get('lastname')
+
+                if lastname is None:
+                    return Response(status=400)
+                else:
+                    query_url = UrlData.objects.raw("""
+                        SELECT * FROM microservice_urldata 
+                        JOIN microservice_urldata ms ON microservice_urldata.url=ms.url
+                        WHERE microservice_urldata.lastname='%s'
+                        GROUP BY microservice_urldata.url
+                    """ % (lastname))
+
+                    urls = [i.url for i in query_url]
+
+                    return Response({"urls": urls})
+            else:
+                return Response({'error': 'Неверный ключ'}, status=401)
+        else:
+            return Response({'error': 'Неверный ключ'}, status=401)
